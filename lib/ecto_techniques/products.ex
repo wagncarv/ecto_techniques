3e99fd1038c8d7b52534f4640647bdea42cdb60a
wagner <wagncarv@gmail.com>
2022-01-14 18:02:12 -0300

    subquery ecto

--- lib/ecto_techniques/products.ex
+++ lib/ecto_techniques/products.ex
@@ -1,6 +1,7 @@
 defmodule EctoTechniques.Products do
     alias EctoTechniques.{Product, Repo}
     import Ecto.Query, only: [from: 2]
+    alias Ecto.Query
 
     def create_many_products(number \\ 5) do
         Enum.map(1..number, &build_product/1)
@@ -83,6 +84,44 @@ defmodule EctoTechniques.Products do
         select: [name: p.name, price: p.price],
         order_by: [desc: :name],
         limit: 3
+        Repo.all(query)
+    end
+
+    def using_where_like do
+        query = from p in Product,
+        select: p,
+        where: like(p.name, "%r%")
+        Repo.all(query)
+    end
+
+    def using_where_like_prefix(filter \\ "u") do
+        filter = "%#{filter}%"
+        query = from p in Product,
+        select: p,
+        where: like(p.name, ^filter)
+        Repo.all(query)
+    end
+
+    def using_group_by do
+        query = from p in Product,
+        select: [name: p.name, category: p.category, price: p.price, quantity: count(p.name)],
+        group_by: [p.category, p.price, p.name]
+        Repo.all(query)
+    end
+
+    def using_min do
+        query = from p in Product,
+        select: [price: min(p.price), name: p.name, count: count(p.name)],
+        limit: 10,
+        group_by: [:price, :name]
+        Repo.all(query)
+    end
+
+    def using_subquery do
+        query = from p in Product,
+        select: p,
+        limit: 10,
+        where: p.price in subquery(from sqp in Product, select: min(sqp.price))
         Repo.all(query)
     end
 end
